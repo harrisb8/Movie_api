cd02364e58a03b776bc022f2d1ff1f3ae06e53cc
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2022-05-28 14:18:59 -0400

    msg

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const { check, validationResult } = require('express-validator');
   Users = Models.User;
  
 
- // mongoose.connect('mongodb://localhost:27017/myFlixDB', {
-   // useNewUrlParser: true,
-  //  useUnifiedTopology: true 
-  //});
+  mongoose.connect('mongodb+srv://test_user:test_user@cluster0.pzp56.mongodb.net/myFlixDB?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+  });
 
-  mongoose.connect( process.env.CONNECTION_URI, 
-  { useNewUrlParser: true, useUnifiedTopology: true });
+ /* mongoose.connect( process.env.CONNECTION_URI, 
+  { useNewUrlParser: true, useUnifiedTopology: true }); */
 
     
 
